a226cc479ca630562d5d6258b5008d457edd61d5
dimarogkov <webdevs1.dima.rogkov@gmail.com>
2020-08-19 19:55:01 +0300

    vue task

--- Vue JS Core/vue_lesson1/src/main.js
+++ Vue JS Core/vue_lesson1/src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
-import Rogkov from './Rogkov.vue'
+import TodoList from './TodoList.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(Rogkov),
+  render: h => h(TodoList),
 }).$mount('#app')
