1d345c1b226ce53b8bce6a55a7b9630f8f129534
dimarogkov <webdevs1.dima.rogkov@gmail.com>
2020-10-11 09:12:55 +0300

    new

--- Angular JS Core/lesson8/src/app/components/user/all-users/all-users.component.ts
+++ Angular JS Core/lesson8/src/app/components/user/all-users/all-users.component.ts
@@ -14,29 +14,24 @@ export class AllUsersComponent implements OnInit {
   user: User;
 
   userId: any;
-
   userForma: FormGroup;
 
   constructor(private userService: UsersService, private formBuilder: FormBuilder) {
-    this.userService.getUsers().subscribe (users => {
-      this.users = users;
-    })
+    this.userService.getUsers().subscribe (users => this.users = users);
   }
 
   ngOnInit(): void {
   
   }
 
-
   getUser() {
     this.userService.getUser(this.userId).subscribe(user => {
       this.user = user;
 
       this.userForma = this.formBuilder.group({
-        name: [user.name, [Validators.required, Validators.minLength(5), Validators.maxLength(25)]],
-        surname: [user.username, [Validators.required, Validators.minLength(5), Validators.maxLength(25)]],
-        email: [user.email, [Validators.required,Validators.email, Validators.minLength(6), Validators.maxLength(33)]],
-        id: [user.id, [Validators.required,Validators.pattern('([0-9]|1[0-9]|2[0-4])')]]
+        name: [user.name, [Validators.required, Validators.minLength(5)]],
+        surname: [user.username, [Validators.required, Validators.minLength(5)]],
+        email: [user.email, [Validators.required,Validators.email, Validators.minLength(6)]]
       })
     })
   }
