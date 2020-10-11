2cafc900126e350ad49633e125df5d0d772b490b
dimarogkov <webdevs1.dima.rogkov@gmail.com>
2020-10-11 09:19:02 +0300

    new

--- Angular JS Core/lesson8/src/app/app.module.ts
+++ Angular JS Core/lesson8/src/app/app.module.ts
@@ -4,15 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AllUsersComponent } from './components/user/all-users/all-users.component';
-import { UserComponent } from './components/user/user/user.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AllUsersComponent,
-    UserComponent
+    AllUsersComponent
   ],
   imports: [
     BrowserModule,
