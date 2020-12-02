44d75ed6c1a15420bfcc54fc5aee6ed75f2891be
dimarogkov <webdevs1.dima.rogkov@gmail.com>
2020-12-02 18:33:46 +0200

    fix

--- Noda JS Core/lesson3/app.js
+++ Noda JS Core/lesson3/app.js
@@ -2,10 +2,11 @@ const express = require('express');
 const { usersRouter } = require('./routes');
 
 const app = express();
+const port = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use('/users', usersRouter);
 
-app.listen(3000);
+app.listen(port);
